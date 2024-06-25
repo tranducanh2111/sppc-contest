c0c9d8f1889664e2eb183ad8a057e0155d6bf679
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-06-25 13:09:23 +1000

    responsive component/event-detail

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -8,8 +8,30 @@ import ContactCard from '@/components/ContactCard'
 
 // Resources for this location page's components
 import LocationHeroImage from '../../public/location-hero.png'
+import EventDetail from '@/components/EventDetail'
 
-const Home: React.FC = () => {
+// Resources for EventDetail Component
+import TimeLineImage from '../../public/timeline.png'
+import CostImage from '../../public/cost.png'
+
+const CardData = [
+  {
+    "name": "Timeline",
+    "descriptions": "Embark on an exhilarating journey through our contest timeline, filled with exciting challenges!",
+    "testURL": "/timeline",
+    "image": TimeLineImage
+  },
+  {
+    "name": "Cost",
+    "descriptions": "Experience our contest with budget-friendly fees, and the opportunity to win fantastic rewards!",
+    "testURL": "/fee",
+    "image": CostImage
+  }
+
+  // Add more entries as needed
+]
+
+const Location: React.FC = () => {
 
   return (
     <Fragment>
@@ -23,9 +45,10 @@ const Home: React.FC = () => {
                 <VenueDetail />
                 <ContactCard />
             </div>
+            <EventDetail cardData={CardData}/>
         </div>
     </Fragment>
   )
 }
 
-export default Home
+export default Location
