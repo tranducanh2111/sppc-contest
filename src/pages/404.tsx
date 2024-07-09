f8f86300da6cda6a61656f8961df1e6229191982
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-07-09 23:12:07 +1000

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,12 +6,12 @@ import Button from '@/components/Button';
 
 const Custom404 = () => {
   return (
-    <section className="lg:flex items-center justify-center min-h-[80vh] gap-x-[3rem]">
+    <section className="max-w-[1570px] mx-auto px-5 lg:flex items-center justify-center min-h-[80vh] gap-x-[3rem]">
         <figure className='w-[70%] mx-auto md:w-[60%] lg:w-[35%] mb-[4rem] lg:mb-0 h-full'>
           <Image src={NotFoundImage} alt="404 Not Found" className='w-full h-full object-cover' />
         </figure>
         <div className='flex flex-col items-center'>
-          <h1 className="text-center text-3xl md:text-h2 lg:text-h1 font-bold text-gray-800 uppercase tracking-widest mb-[0.75rem]">AWWW...Don&apos;t cry.</h1>
+          <h1 className="text-center text-3xl md:text-h2 font-bold text-gray-800 uppercase tracking-widest mb-[0.75rem]">AWWW...Don&apos;t cry.</h1>
           <p className="text-gray-600 text-xl md:text-h3 mb-[0.25rem] text-center">It&apos;s just a 404!</p>
           <p className="text-gray-600 text-xl md:text-h3 mb-[2.25rem] text-center">What you are looking for may have been misplaced in Long Term Memory...</p>
           <Link href="/">
